7db2f9c9b79a55c7b22de1088bd368d21436ab25
mari-mullen <marilenamullen@gmail.com>
2025-01-21 19:54:22 -0800

    nav menu
    added hamburger nav menu to all 4 pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 console.log("Welcome to My Portfolio!");
+
+$(document).ready(function () {
+  $('#hamburger').on('click', function () {
+    $('#mainNav').toggleClass('active');
+  });
+
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    var target = this.hash;
+    $('html, body').animate(
+      { scrollTop: $(target).offset().top },
+      600
+    );
+  });
+});
